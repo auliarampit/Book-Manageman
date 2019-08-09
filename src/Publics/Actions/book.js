5aaa7217cd0ba18e4d6c36840068a86f7415c9f1
auliarampit <auliarahmat25599@gmail.com>
2019-08-09 10:58:05 +0700

    access

--- src/Publics/Actions/book.js
+++ src/Publics/Actions/book.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getBook = () => {
     return {
         type: 'GET_BOOK',
-        payload: axios.get(`http://192.168.6.104:8082/nameBook` ,{
+        payload: axios.get(`https://library-backend-ar.herokuapp.com/nameBook` ,{
             headers: {
                 authorization: 'x-header',
                 
@@ -15,7 +15,7 @@ export const getBook = () => {
 export const getPinjam = (idBook) => {
     return {
         type: 'GET_PINJAM',
-        payload: axios.get(`http://192.168.6.104:8082/Pinjam/${idBook}`, {
+        payload: axios.get(`https://library-backend-ar.herokuapp.com/Pinjam/${idBook}`, {
             headers: {
                 authorization: 'x-header'
             }
@@ -27,7 +27,7 @@ export const getPinjam = (idBook) => {
 export const Pinjam = (idCard) => {
     return {
         type: 'PINJAM',
-        payload: axios.get(`http://192.168.6.104:8082/Pinjam/?idCard=${idCard}`, {
+        payload: axios.get(`https://library-backend-ar.herokuapp.com/Pinjam/?idCard=${idCard}`, {
             headers: {
                 authorization: 'x-header'
             }
@@ -39,7 +39,7 @@ export const Pinjam = (idCard) => {
 export const postBook = (formData) => {
     return {
         type: 'POST_BOOK',
-        payload: axios.post(`http://192.168.6.104:8082/nameBook`,formData, {
+        payload: axios.post(`https://library-backend-ar.herokuapp.com/nameBook`,formData, {
             headers: {
                 authorization: 'x-header',
                 'x-control-user' : localStorage.iduser,
@@ -53,7 +53,7 @@ export const postBook = (formData) => {
 export const postPinjam = (data) => {
     return {
         type: 'POST_PINJAM',
-        payload: axios.post(`http://192.168.6.104:8082/Pinjam/post`,data),
+        payload: axios.post(`https://library-backend-ar.herokuapp.com/Pinjam/post`,data),
     }
 }
 
@@ -61,7 +61,7 @@ export const deleteBook = (idBook, token, iduser) => {
     console.log(idBook,token,iduser)
     return {
         type: 'DELETE_BOOK',
-        payload: axios.delete(`http://192.168.6.104:8082/nameBook/${idBook}`,null,{
+        payload: axios.delete(`https://library-backend-ar.herokuapp.com/nameBook/${idBook}`,null,{
             headers: {
                 authorization: 'x-header',
                 'x-control-user': localStorage.iduser,
@@ -74,7 +74,7 @@ export const deleteBook = (idBook, token, iduser) => {
 export const updateBook = (data,id) => {
     return {
         type: 'UPDATE_BOOK',
-        payload: axios.patch(`http://192.168.6.104:8082/nameBook/${id}`,data, {
+        payload: axios.patch(`https://library-backend-ar.herokuapp.com/nameBook/${id}`,data, {
             headers: {
                 authorization: 'x-header',
                 'x-control-user' : localStorage.iduser,
@@ -88,6 +88,6 @@ export const patchPinjam = (idPinjam, idBook) => {
     console.log(idPinjam, idBook)
     return {
         type: 'UPDATE_PINJAM',
-        payload: axios.patch(`http://192.168.6.104:8082/Pinjam/${idPinjam}`,{idBook:idBook})
+        payload: axios.patch(`https://library-backend-ar.herokuapp.com/Pinjam/${idPinjam}`,{idBook:idBook})
     }
 }
--- src/Publics/Actions/login.js
+++ src/Publics/Actions/login.js
@@ -3,13 +3,13 @@ import axios from 'axios'
 export const register = (data) => {
     return {
         type: 'POST_REGISTER',
-        payload: axios.post(`http://192.168.6.104:8082/login/post`, data)
+        payload: axios.post(`https://library-backend-ar.herokuapp.com/login/post`, data)
     }
 }
 
 export const login = (data) => {
     return {
         type: 'GET_LOGIN',
-        payload: axios.post(`http://192.168.6.104:8082/login/login`,data)
+        payload: axios.post(`https://library-backend-ar.herokuapp.com/login/login`,data)
     }
 }
